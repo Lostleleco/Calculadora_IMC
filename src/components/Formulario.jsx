d14e1f4b22e015b19bbc67a7a34ddd27b303d279
Alex <alex_soares_oliveira@hotmail.com>
2024-08-01 18:46:05 -0300

    alteração de informação

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,6 +5,7 @@ function Formulario() {
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
   const [imc, setImc] = useState(null);
+  const [mensagem, setMensagem] = useState('');
 
   const calcularIMC = (peso, altura) => {
     if (peso > 0 && altura > 0) {
@@ -13,12 +14,25 @@ function Formulario() {
     return 0;
   };
 
+  const obterMensagemIMC = (imc) => {
+    if (imc < 18.5) {
+      return 'Você está abaixo do peso.';
+    } else if (imc >= 18.5 && imc < 24.9) {
+      return 'Você está com peso normal.';
+    } else if (imc >= 25 && imc < 29.9) {
+      return 'Você está com sobrepeso.';
+    } else {
+      return 'Você está com obesidade.';
+    }
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const pesoNum = parseFloat(peso);
     const alturaNum = parseFloat(altura);
     const resultadoIMC = calcularIMC(pesoNum, alturaNum);
-    setImc(resultadoIMC.toFixed(2)); 
+    setImc(resultadoIMC.toFixed(2));
+    setMensagem(obterMensagemIMC(resultadoIMC));
   };
 
   return (
@@ -50,6 +64,7 @@ function Formulario() {
         {imc !== null && (
           <div className="result">
             <h2>O Valor do seu IMC é: {imc}</h2>
+            <p>{mensagem}</p>
           </div>
         )}
       </form>
